075cf95beb9153bf036cf905a15605109d673c4f
popdf10 <popdf10@gmail.com>
2020-04-17 12:58:39 +0900

    Finished Flask Web Scrapper!

--- SuperScrapper.py
+++ SuperScrapper.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
+from scrapper import get_jobs
+from exporter import save_to_file
 
 app = Flask("SuperScrapper")
+
+db = {}
 
 
 @app.route('/')
@@ -11,7 +15,38 @@ def home():
 @app.route('/report')
 def report():
     word = request.args.get("word")
-    return render_template("report.html", SearchingBy=word)
+    if word:
+        word = word.lower()
+        existingDb = db.get(word)
+        if existingDb:
+            jobs = existingDb
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        return redirect('/')
+    return render_template("report.html", SearchingBy=word, resultsNumber=len(jobs), jobs=jobs)
+
+
+@app.route('/export')
+def export():
+    try:
+        word = request.args.get("word")
+        print(word)
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        print(jobs)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv",
+                         mimetype='text/csv',
+                         attachment_filename=f"{word}.csv",
+                         as_attachment=True)
+    except:
+        return redirect('/')
 
 
 app.run(host="127.0.0.1")
